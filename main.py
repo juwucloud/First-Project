584b01ec616fd03e5460915e0aaf0ade08dda08d
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-17 11:45:42 +0200

    [EDIT] Commented the Testing out and sorted the Functions to get ready for User-Input

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
+
+# Define Functions
+#Addition - Function
 def addition (a, b):
         return a + b
 
-addition_result = addition(5,7)
-print(f"The addition result is : {addition_result}")
-
-def multiplication(a, b):
-        return a * b
-
-multiplication_result = multiplication(5,7)
-print(f"the multiplication result is: {multiplication_result}")
-
+#Substraction - Function
 def substraction(a, b):
         return a - b
 
-substraction_result = substraction(7,5)
-print(f"The substraction result is: {substraction_result}")
+#Multiplication - Function        
+def multiplication(a, b):
+        return a * b
 
+#Devision - Function
 def devision(a, b):
         if b == 0:
                 print("You can not devide by 0")
         else:
                 return a / b 
+
         
-devision_result = devision(8,4)
-print(f"The devision result is: {devision_result}")
+
+
+
+
+# Function testing Area
+#-----------------------
+# addition_result = addition(5,7)
+# print(f"The addition result is : {addition_result}")
+# multiplication_result = multiplication(5,7)
+# print(f"the multiplication result is: {multiplication_result}")
+# substraction_result = substraction(7,5)
+# print(f"The substraction result is: {substraction_result}")
+# devision_result = devision(8,4)
+# print(f"The devision result is: {devision_result}")
+
