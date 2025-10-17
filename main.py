2ee0a9012d8c29921dbc9aea15ffe94a522f9e72
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-17 14:52:02 +0200

    [ADD] packed the calculator in a function for the possibility to repeat the calculation as many times as you want

--- main.py
+++ main.py
@@ -15,57 +15,74 @@ def multiplication(a, b):
 def division(a, b):
         return a / b 
 
-# Start of the Calculator
-print("Welcome to the Calculator Application")
-print("You can add, substract, multipy and divide two numbers!\n")
+def calculator():
+        # Start of the Calculator
+        print("Welcome to the Calculator Application")
+        print("You can add, substract, multipy and divide two numbers!\n")
 
-# User Input for Operator Choice /w validity check
+        # User Input for Operator Choice /w validity check
+        while True:
+                operatorChoice = input("What do you want to do?\n+ for Addition\n- for Substraction\n* for Multiplication\n/ for Division\n\n")
+
+                if operatorChoice == "+" or operatorChoice == "-" or operatorChoice == "*" or operatorChoice == "/":
+                        print("")
+                        print(f"Your Choice is: {operatorChoice}\n")
+                        break
+                else:
+                        print("Invalid Input, try again!\n\n")
+
+        # User Input for Number Choice
+        print("Now it is time to choose your numbers. Decimals are allowed!\n")
+
+        # checking if Input is valid (repeats the input query when not a number)
+        while True:
+                try:
+                        InputNumberA = float(input("What is your first number? "))
+                        print("")
+
+                        while True: # Second loop to check if division by 0
+                                InputNumberB = float(input("What is your second number? "))
+                                if operatorChoice == "/" and InputNumberB == 0:
+                                        print("You cannot divide by 0. Please enter another number!\n")
+                                else:
+                                        break
+                        break
+
+                except ValueError:
+                        print("Invalid Input, try again!\n")
+
+
+        print(f"Your first number is {InputNumberA} and your second number is {InputNumberB}\n")
+        print("Calculating ... \n")
+
+
+        # Calculting the Result
+        if operatorChoice == "+":
+                result = addition(InputNumberA, InputNumberB)
+        elif operatorChoice == "-":
+                result = substraction(InputNumberA, InputNumberB)
+        elif operatorChoice == "*":
+                result = multiplication(InputNumberA, InputNumberB)
+        elif operatorChoice == "/":
+                result = division(InputNumberA, InputNumberB)
+
+        print(f"Your Result: {InputNumberA} {operatorChoice} {InputNumberB} = {result}\n")
+
+
+# Program starts!
+calculator()
+
 while True:
-        operatorChoice = input("What do you want to do?\n+ for Addition\n- for Substraction\n* for Multiplication\n/ for Division\n\n")
-
-        if operatorChoice == "+" or operatorChoice == "-" or operatorChoice == "*" or operatorChoice == "/":
-                print("")
-                print(f"Your Choice is: {operatorChoice}\n")
+        again = input ("Do you want to calculate again? (y/n)" ).lower()
+        if again == "y":
+                calculator()
+        elif again == "n":
+                print("\n/Thank you for using the calculator. See you next time!")
                 break
         else:
-                print("Invalid Input, try again!\n\n")
-
-# User Input for Number Choice
-print("Now it is time to choose your numbers. Decimals are allowed!\n")
-
-# checking if Input is valid (repeats the input query when not a number)
-while True:
-        try:
-                InputNumberA = float(input("What is your first number? "))
-                print("")
-
-                while True: # Second loop to check if division by 0
-                        InputNumberB = float(input("What is your second number? "))
-                        if operatorChoice == "/" and InputNumberB == 0:
-                                print("You cannot divide by 0. Please enter another number!\n")
-                        else:
-                                break
-                break
-
-        except ValueError:
-               print("Invalid Input, try again!\n")
+                print("Invalid input, please type 'y' or 'n'.")
 
 
-print(f"Your first number is {InputNumberA} and your second number is {InputNumberB}\n")
-print("Calculating ... \n")
-
-
-# Calculting the Result
-if operatorChoice == "+":
-    result = addition(InputNumberA, InputNumberB)
-elif operatorChoice == "-":
-    result = substraction(InputNumberA, InputNumberB)
-elif operatorChoice == "*":
-    result = multiplication(InputNumberA, InputNumberB)
-elif operatorChoice == "/":
-    result = division(InputNumberA, InputNumberB)
-
-print(f"Your Result: {InputNumberA} {operatorChoice} {InputNumberB} = {result}")
 
 
 # Function testing Area
