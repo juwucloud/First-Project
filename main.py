f0e14d6b64db9e1d3f45bcb561f49e42718477b1
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-17 10:39:51 +0200

    [ADD]New multiplication feature added

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def addition (a, b):
 addition_result = addition(5,7)
 print(f"The addition result is : {addition_result}")
 
+def multiplication(a, b):
+        return a*b
+
+multiplication_result = multiplication(5*7)
+print(f"the multiplication result is: {multiplication_result}")
+
