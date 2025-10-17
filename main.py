d2197d4757f0428561d597bb376715186a91bb7c
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-17 10:41:14 +0200

    [FIX]corrected multiplication error

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ addition_result = addition(5,7)
 print(f"The addition result is : {addition_result}")
 
 def multiplication(a, b):
-        return a*b
+        return a * b
 
-multiplication_result = multiplication(5*7)
+multiplication_result = multiplication(5,7)
 print(f"the multiplication result is: {multiplication_result}")
 
